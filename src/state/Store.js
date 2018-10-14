8f4f75c10f50e1505ae5d41070b1bcb546e8973b
Vinicius Santana <santanaviny@gmail.com>
2018-10-14 01:06:43 -0700

    Redux dev tools middleware setup

--- src/state/Store.js
+++ src/state/Store.js
@@ -1,7 +1,13 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk';
 import todoApp from './reducers'
 
-const store = createStore(todoApp, applyMiddleware(thunk))
+const store = createStore(
+    todoApp, 
+    compose(
+        applyMiddleware(thunk),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+      )
+)
 
 export default store
