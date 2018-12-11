11b83bfa91484c65ec26ebeb5c1fb8a21f32b160
Vinicius Santana <santanaviny@gmail.com>
2018-12-10 17:52:07 -0800

    Updates

---
+++ precache-manifest.556df73c7e3811cd5e5bf7fbd650bfb1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "10c83e7ac815c4dfa337",
+    "url": "/redux-todo/static/js/main.10c83e7a.chunk.js"
+  },
+  {
+    "revision": "5a8b35e73f2658c2ae01",
+    "url": "/redux-todo/static/js/1.5a8b35e7.chunk.js"
+  },
+  {
+    "revision": "e97dda427c85cde9b7b5",
+    "url": "/redux-todo/static/js/runtime~main.e97dda42.js"
+  },
+  {
+    "revision": "f3c20ca00c854ca5c635eb1e6e0fbc97",
+    "url": "/redux-todo/index.html"
+  }
+];
